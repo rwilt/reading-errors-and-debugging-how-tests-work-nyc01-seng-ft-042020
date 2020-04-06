37b1d8ac759bfb5f55321064eed03c5004dc3494
rosie w <rosie.wilt@gmail.com>
2020-04-06 18:55:42 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
 def calculator(num1, num2)
-  first_number= num1
+  first_number= 2
   second_number = num2
   sum = first_number + second_number
   difference = first_number - second_number
