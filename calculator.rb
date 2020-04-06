9c5752424da260a4a452421d090192d6af3b8396
rosie w <rosie.wilt@gmail.com>
2020-04-06 18:58:12 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,9 @@
 # Add your variables here
 first_number = 4
+
 def calculator(num1, num2)
   first_number= get_variable_from_file('./calculator.rb', "first_number")
-  second_number = num2
+  second_number = 4
   sum = first_number + second_number
   difference = first_number - second_number
   product = first_number * second_number
